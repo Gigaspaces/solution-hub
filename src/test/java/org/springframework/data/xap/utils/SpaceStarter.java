6328017c9a139f415986bba8c1ed16a2e7cb14d2
Pavlo Romanenko <Pavlo_Romanenko@epam.com>
2015-02-19 12:40:13 +0200

    Stop embedded space if it was started as a part of maven build

--- src/test/java/org/springframework/data/xap/utils/SpaceStarter.java
+++ src/test/java/org/springframework/data/xap/utils/SpaceStarter.java
@@ -11,9 +11,23 @@ import static org.springframework.data.xap.utils.TestUtils.*;
 
 public class SpaceStarter {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         GigaSpace gigaspace = new GigaSpaceConfigurer(
                 new UrlSpaceConfigurer("/./space?groups=" + getGroupName())).gigaSpace();
+        // if started from maven wit some time for tests execution and exit
+        if (mavenTest(args)){
+            Thread.sleep(60000);
+            System.exit(0);
+        }
+    }
+
+    /**
+     * SpaceStarter can be run as main class for debug purposes or as a part of maven build
+     * @param args
+     * @return
+     */
+    private static boolean mavenTest(String[] args) {
+        return args.length  > 0 && "test".equals(args[0]);
     }
 
 }
