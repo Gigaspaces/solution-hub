3535fa8f798b25e45af030e8daa8608b315678a1
Oleksiy Dyagilev <oleksiy_dyagilev@epam.com>
2015-02-20 15:20:26 +0200

    move code to own directory

--- src/test/java/org/springframework/data/xap/integration/manual/PersonRepositoryExtended.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.data.xap.integration.manual;
-
-import org.springframework.data.xap.repository.PersonRepository;
-
-/**
- * @author Leonid_Poliakov
- */
-public interface PersonRepositoryExtended extends PersonRepository, PersonRepositoryCustom {
-}
--- src/test/java/org/springframework/data/xap/model/TeamStatus.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.data.xap.model;
-
-/**
- * @author Leonid_Poliakov
- */
-public enum TeamStatus {
-    ACTIVE, INACTIVE, UNKNOWN
-}
